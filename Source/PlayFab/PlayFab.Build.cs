9b362c0243f89a784d751c9e209cffcfd8e3c941
Ata Çetin <admin@ihavenick.com>
2015-06-28 03:03:16 +0300

    Update PlayFab.Build.cs
    Fixed compile error PlayFab.Build.cs(19,16): error CS1520: Yöntemin bir dönüş türü olmalıdır " (The method need to have a return value)

--- Source/PlayFab/PlayFab.Build.cs
+++ Source/PlayFab/PlayFab.Build.cs
@@ -16,7 +16,7 @@ namespace UnrealBuildTool.Rules
             get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
         }
 
-        public PlayFabPlugin(TargetInfo Target)
+        public PlayFab(TargetInfo Target)
         {
             PrivateIncludePaths.AddRange(
                 new string[] {
